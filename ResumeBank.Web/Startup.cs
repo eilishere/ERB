f9bab235433db835031780b66c547a182999cecb
Rishad Wahid <rbwahid@gmail.com>
2018-11-10 00:35:13 +0600

    Initial Commit.

---
+++ ResumeBank.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ResumeBank.Web.Startup))]
+namespace ResumeBank.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
